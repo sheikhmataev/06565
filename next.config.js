355f98b01016c475b080dbbc3d1ba7113229b8a4
Abdulsamad Sheikh <abdulsas@stud.ntnu.no>
2025-10-21 22:48:52 +0200

    Configure for GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const isGithubActions = process.env.GITHUB_ACTIONS === 'true';
+const repoName = '06565';
+
+const nextConfig = {
+  output: 'export',
+  basePath: isGithubActions ? `/${repoName}` : '',
+  assetPrefix: isGithubActions ? `/${repoName}/` : '',
+  images: {
+    unoptimized: true,
+  },
+}
 
 module.exports = nextConfig
